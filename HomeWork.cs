3bf81e117e005cbb6113d4827935af3afb885d9f
hvmegy <hamegy08lleo@gmail.com>
2024-01-26 11:34:40 +0700

    NamHy code them HomeWork

--- HomeWork.cs
+++ HomeWork.cs
@@ -8,7 +8,9 @@ namespace test_git
 {
     internal class HomeWork
     {
-        //Lưu Vĩnh Tường code cái này
-        //Nguyễn Nam Hy code cái này luôn
+        for (int i = 1; i <= 10; i++) 
+            { 
+                cout << "MUNG NGAY CHUA SINH RA DOI";
+            }
     }
 }
